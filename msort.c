bc897f691d98f7b06088f25f9aeaa84bdc432773
Sawyer Mifsud <wingren013@gmail.com>
2017-06-20 15:29:21 -0700

    fixed lacking comparison function

--- msort.c
+++ msort.c
@@ -18,7 +18,7 @@ void	msort_swap(size_t size, char *a, size_t i, size_t l)
 	free(t);
 }
 
-void	*tmsort(void *sort, size_t count, size_t datasize)
+void	*tmsort(void *sort, size_t count, size_t datasize, int (*cmp) (void *, void *))
 {
 	//timsort
 	return (sort);
@@ -161,7 +161,7 @@ void	*qmsort(void *params)
 	return (sorts);
 }
 
-void	*msort(void *sort, size_t count, size_t datasize)
+void	*msort(void *sort, size_t count, size_t datasize, int (*cmp) (void *, void *))
 {
 	size_t		pi;
 	size_t		i;
@@ -191,7 +191,7 @@ void	*msort(void *sort, size_t count, size_t datasize)
 			l--;
 		if (i >= l)
 			break ;
-		if (i == pi)void	msort(t_sortbod *sorts, size_t count, size_t datasize)
+		if (i == pi)
 			pi = l;
 		else if (l == pi)
 			pi = i;
--- msort.h
+++ msort.h
@@ -2,6 +2,7 @@
 # define MSORT_H
 
 # define THREAD_THRESHOLD 10000
+# define SORTSWITCH_THRESH 100000
 
 #include <stdlib.h>
 
@@ -14,7 +15,7 @@ typedef struct	msort_param_s
 	int			(*cmp) (const void *, const void *);
 }		msort_param_t;
 
-void		*msort(void *sorts, size_t count, size_t datasize);
-void		*tmsort(void *sorts, size_t count, size_t datasize);
+void		*msort(void *sorts, size_t count, size_t datasize, int (*cmp) (void *, void *));
+void		*tmsort(void *sorts, size_t count, size_t datasize, int (*cmp) (void *, void *));
 
 #endif
